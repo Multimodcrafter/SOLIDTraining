d107d54fbed3a9e1c70b3f80ab1a3ec23f8b2f94
CodeShareRepeat <rckoenig@gmx.de>
2017-11-13 10:20:22 +0100

    Initiale Erstellung

---
+++ LetterSWrong/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LetterSWrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step1: Ist File vorhanden
+
+            // Step2: Lese File 
+
+            // Step2.1: Create Array from file content
+            
+            // Step3: Forach mit min. 2 Fallunterscheidung
+
+            // Step Send Mail
+
+       
+           
+
+        }
+    }
+}
