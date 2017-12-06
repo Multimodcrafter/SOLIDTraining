df522098e9c84d6aa8aab6901d9ed9416af7cd05
Philipp Häfelfinger <philipp.haefelfinger@isolutions.ch>
2017-12-06 14:50:10 +0100

    added solid letter L for Liskov substitution

---
+++ src/LetterLRight/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LetterLRight
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("1 = Rectangle, 2 = Square");
+
+            var userDecision = Console.ReadLine();
+
+            if (userDecision == "1")
+            {
+                var rectangle = new Rectangle(10, 10);
+                ValidateLiskov(rectangle, 100);
+                rectangle.Width = 20;
+                ValidateLiskov(rectangle, 200);
+            }
+            else
+            {
+                var rectangle = new Square(10);
+                ValidateLiskov(rectangle, 100);
+                rectangle.Length = 20;
+                ValidateLiskov(rectangle, 400);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void ValidateLiskov(Figure figure, int expectedArea)
+        {
+            var area = figure.CalculateArea();
+            Console.WriteLine($"Area is {area} - expected {expectedArea}");
+            var foregroundColor = Console.ForegroundColor;
+            try
+            {
+                if (area != expectedArea)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Liskov violated because Height should not have been modified.");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Liskov principal handled correctly.");
+                }
+            }
+            finally
+            {
+                Console.ForegroundColor = foregroundColor;
+            }
+        }
+    }
+}
---
+++ src/LetterLRight/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LetterLRight
+{
+    public class Square : Figure
+    {
+        /// <summary>Initializes a new instance of the <see cref="T:System.Object"></see> class.</summary>
+        public Square(int length)
+        {
+            Length = length;
+            Console.WriteLine($"Creating {typeof(Square).FullName} with length of {length}");
+        }
+
+        public int Length { get; set; }
+
+        public override int CalculateArea()
+        {
+            return Length * Length;
+        }
+    }
+}
